8da7f7d0e8447aeaf3d2ab27c0f954f51296d895
Kyungmin.Noh <blackcata109@gmail.com>
2024-08-23 09:17:39 -0400

    Add python script of converting data from month to annual

--- convert_mon_to_ann.sh
+++ convert_mon_to_ann.sh
@@ -12,21 +12,23 @@
 # Set specific varibles, years, file type
 # 	1. Just change the upper setting part only and don't change below codes
 
-# export var=("chlos" "dfeos" "dissicos" "no3os" "intpp" "fgco2" "epc100" "spco2")
-# export var=("chl" "dfe" "dissic" "no3" "o2" "ph" "pp") ### ocean_cobalt_omip_tracers_month_z                  
-# export var=("limpdiat" "limpdiaz" "limppico" "limpmisc" "limndiat" "limndiaz" "limnpico" "limnmisc" "limfediat" "limfediaz" "limfepico" "limfemisc" "intbfe" "intppdiat" "intppdiaz" "intppmisc" "intpppico")
-#export var=("thetao" "so")
-export var=("fgco2")
-export yr_strt=2015                  # Start year of regrdding
-export yr_end=2100                  # End year of regridding
+# export var=("intpp" "epc100" "spco2" "chlos" "dfeos" "dissicos" "no3os" "po4os")
+# export var=("intpp" "epc100" "spco2")
+export var=("tos")
+#export var=("chl" "dfe" "dissic" "no3" "o2" "ph" "pp") ### ocean_cobalt_omip_tracers_month_z                  
+#export var=("intpn2" "limpdiat" "limpdiaz" "limppico" "limpmisc" "limndiat" "limndiaz" "limnpico" "limnmisc" "limfediat" "limfediaz" "limfepico" "limfemisc" "intbfe" "intppdiat" "intppdiaz" "intppmisc" "intpppico")
+#export var=("dfe" "dissic" "no3" "po4" "o2" "o2sat" "thetao" "so")
+
+export yr_strt=1990                  # Start year of regrdding
+export yr_end=2014                  # End year of regridding
 
 export date="0101"	             # Month & Day of simulation results
-#export exp="historical_D1_c5_dfe_fert_LS_SO_S"
-export exp="esm-ssp585_D1_dfe_fert_GLOBAL_1x_STOP"
+#export exp="esm-hist_D1"
+export exp="historical_D1_c5_dfe_fert_LS_SO_S"
 
 export model="ESM4"    # The name of simulation
-#export input_path="/work/Kyungmin.Noh/DATA/GFDL_ESM4/1.IRON_FERTILIZATION/LARGE_SCALE/SO_S/"
-export input_path="/work/Kyungmin.Noh/DATA/GFDL_ESM4/1.IRON_FERTILIZATION/SSP585/STOP/GLOBAL/STOP_30Y/"
+export input_path="/work/Kyungmin.Noh/DATA/GFDL_ESM4/1.IRON_FERTILIZATION/LARGE_SCALE/SO_S/"
+#export input_path="/archive/Kyungmin.Noh/DATA/GFDL_ESM4/2.PERSISTENCE_OIF/2.SSP585/1.CTRL/"
 
 export output_path=$input_path
 
